1e8ea5d9a94bc80719c9bdcef551e75b570bd648
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-01-28 11:26:47 +0100

    .ready()

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+/*
+.ready() executes ONLY after DOM is fully loaded
+*/
+// $(document).ready(function() {
+//     $("h3").css("text-decoration", "underline"); // underlines all <h2> elements
+//     $("ul").css("border", "solid 1px #ccc") // adds a border to all lists (ul)
+// });
